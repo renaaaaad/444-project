511c33cd0aaf88ecea0c42ba23dcbdc193ec297a
nora <noraalnjeem@gmail.com>
2019-02-08 20:07:16 +0300

    Nora

---
+++ app/src/main/java/com/example/renad/exchangeit/searchResult.java
@@ -0,0 +1,19 @@
+package com.example.renad.exchangeit;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class searchResult extends Fragment {
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            // Inflate the layout for this fragment
+            return inflater.inflate(R.layout.search_result_fragment, container, false);
+        }
+
+    }
+
